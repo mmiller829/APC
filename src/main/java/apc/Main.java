b3fec14161ee377809ab0d136ce9acf55760af3f
mmiller829 <crunchnotable@gmail.com>
2018-05-11 20:59:35 -0400

    pom file and log4j

--- src/main/java/apc/Main.java
+++ src/main/java/apc/Main.java
@@ -4,11 +4,20 @@
 
 package apc;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 public class Main
 {
-
+    final static Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args)
     {
+        logger.trace("trace");
+        logger.debug("debug");
+        logger.info("info");
+        logger.error("error");
+        logger.fatal("fatal");
     }
 
 }
