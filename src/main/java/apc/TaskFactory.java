122bebb857362c044f7c668b655ee243b170d5cc
mmiller829 <crunchnotable@gmail.com>
2018-05-15 01:54:11 -0400

    change in taskFactory

--- src/main/java/apc/TaskFactory.java
+++ src/main/java/apc/TaskFactory.java
@@ -1,11 +1,13 @@
 
 package apc;
 
+import enums.Command;
+
 public class TaskFactory
 {
     public static Task createSocketStateTask(SocketStateManager socketStateManager)
     {
-        return new SocketStateTask(socketStateManager);
+        return new SocketStateTask(Command.GETSOCKETSTATE, socketStateManager);
     }
     
 }
