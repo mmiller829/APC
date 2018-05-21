b7766ca17b47628dea4c39b5052f1e0475508c76
mmiller829 <crunchnotable@gmail.com>
2018-05-21 16:54:33 -0400

    changed global variables

--- src/main/java/apc/GlobalVariables.java
+++ src/main/java/apc/GlobalVariables.java
@@ -3,5 +3,9 @@ package apc;
 
 public class GlobalVariables
 {
+    public static String ipAddress;
+    public static int port;
+    
+    public static final String loginFileName = "login.APC";
     
 }
