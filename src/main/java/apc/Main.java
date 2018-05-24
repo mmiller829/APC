33fcf24353af1ba0643d7048947dfc742f6e0a0f
mmiller829 <crunchnotable@gmail.com>
2018-05-24 03:07:17 -0400

    added darcula jar and updated pom file

--- src/main/java/apc/Main.java
+++ src/main/java/apc/Main.java
@@ -3,8 +3,12 @@
  */
 package apc;
 
+import com.bulenkov.darcula.DarculaLaf;
 import gui.LoginFrame;
+import java.io.File;
+import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.basic.BasicLookAndFeel;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,9 +17,21 @@ public class Main
 
     final static Logger logger = LogManager.getLogger(Main.class);
 
-    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException
+    public static void main(String[] args) throws UnsupportedLookAndFeelException
     {
-        new LoginFrame();
+        BasicLookAndFeel darcula = new DarculaLaf();
+        UIManager.setLookAndFeel(darcula);
+
+        File file = new File(GlobalVariables.loginFileName);
+        if (!file.exists())
+        {
+            new LoginFrame();
+        }
+        else
+        {
+            // log in
+        }
+
     }
 
 }
